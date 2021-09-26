76a17557ba7b70dc6a781f8ebae9c94b21883208
priyanka897-star <priyankamore8997@gmail.com>
2021-09-27 00:42:39 +0530

    Added:Functionality to print welcome Message

---
+++ SnakeAndLadder/SnakeAndLadder/src/com/bridgelab/snakeandladder/SnakeAndLadder.java
@@ -0,0 +1,8 @@
+package com.bridgelab.snakeandladder;
+
+public class SnakeAndLadder {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Snake And Ladder Program");
+	}
+
+}
