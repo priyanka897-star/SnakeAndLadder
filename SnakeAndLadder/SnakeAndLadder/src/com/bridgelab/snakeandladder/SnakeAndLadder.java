6d968d534532790fd798019f604766db7ba65a55
priyanka897-star <priyankamore8997@gmail.com>
2021-09-27 02:21:24 +0530

    Added:Code to start Game with player

--- SnakeAndLadder/SnakeAndLadder/src/com/bridgelab/snakeandladder/SnakeAndLadder.java
+++ SnakeAndLadder/SnakeAndLadder/src/com/bridgelab/snakeandladder/SnakeAndLadder.java
@@ -3,13 +3,10 @@ package com.bridgelab.snakeandladder;
 public class SnakeAndLadder {
 	public static void main(String[] args) {
 		
-	        // one player starting on square one
-	        int Diceplayer = (int)(Math.random()*6 )+1;
-	        int startPosition = 0 ;
-	        if (Diceplayer ==  startPosition ) {
-	        	
-	        	System.out.println ("Player start Game with Position " +Diceplayer);
-	        }
+	         int startPosition = 0 ;
+	       
+	        System.out.println("Player start Game with Position "+startPosition);
+	    
 	        
 	               
 	            
